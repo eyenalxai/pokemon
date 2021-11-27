bfc271842cd8a3f756379798fb6b2a825f1a5811
Yest Izit <hi@dmitry.email>
2021-11-27 15:47:54 +0300

    Remove caveat section, I will write it later when I know what it is and can articulate without being bitter

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,30 +4,26 @@ import { SelectedPokemonTypes } from "../components/SelectedPokemonTypes";
 import { PokemonType } from "../types/PokemonType";
 import { allPokemonNames } from "../util/AllPokemonNames";
 import { SWRResponse } from "swr";
-import { getPokemon } from "../util/GetPokemon";
-import { getPokemonTypes } from "../util/GetPokemonTypes";
-import { Pokemon } from "../types/Pokemon";
+import { usePokemonTypes } from "../util/UsePokemonTypes";
 import { TypeColumns } from "../components/TypeColumns";
 import { useState } from "react";
 import { Select } from "chakra-react-select";
 
 function Home() {
-  const [pokemonOption, setPokemonOption] = useState<PokemonOption>(allPokemonNames[Math.floor(Math.random() * allPokemonNames.length)]);
+    const [pokemonOption, setPokemonOption] = useState<PokemonOption>(allPokemonNames[Math.floor(Math.random() * allPokemonNames.length)]);
 
-  const { data: pokemon }: SWRResponse<Pokemon, Error> = getPokemon(pokemonOption)
-  const { data: pokemonTypes }: SWRResponse<PokemonType[], Error> = getPokemonTypes(pokemon)
+    const { data: pokemonTypes }: SWRResponse<PokemonType[], Error> = usePokemonTypes(pokemonOption)
 
-
-  return (
-      <Container mt={ 12 } maxWidth={ "xs" }>
-        <Select options={ allPokemonNames }
-                onChange={ (x: PokemonOption) => setPokemonOption(x as PokemonOption) }
-                defaultValue={pokemonOption}
-        />
-        { pokemonTypes
-            ? <>
-              <SelectedPokemonTypes pokemonTypes={ pokemonTypes }/>
-              <TypeColumns pokemonTypes={ pokemonTypes }/>
+    return (
+        <Container mt={ 12 } maxWidth={ "xs" }>
+            <Select options={ allPokemonNames }
+                    onChange={ (x: PokemonOption) => setPokemonOption(x as PokemonOption) }
+                    defaultValue={ pokemonOption }
+            />
+            { pokemonTypes
+                ? <>
+                    <SelectedPokemonTypes pokemonTypes={ pokemonTypes }/>
+                    <TypeColumns pokemonTypes={ pokemonTypes }/>
             </>
             : <Center mt={12}>
               <Spinner/>
---
+++ util/UsePokemonTypes.tsx
@@ -0,0 +1,20 @@
+import { Pokemon } from "../types/Pokemon";
+import useSWR, { SWRResponse } from "swr";
+import { PokemonType } from "../types/PokemonType";
+import { pokemonApiUrl, pokemonTypeApiUrl } from "../config/URL";
+import { multiFetcher } from "./MultiFetcher";
+import { PokemonOption } from "../types/PokemonOption";
+import { fetcher } from "./Fetcher";
+
+
+export function usePokemonTypes(pokemonOption: PokemonOption): SWRResponse<PokemonType[], Error> {
+    const { data: pokemon }: SWRResponse<Pokemon, Error> = useSWR(
+        `${ pokemonApiUrl }/${ pokemonOption.value }`, fetcher
+    )
+
+    return useSWR(pokemon !== undefined
+            ? pokemon.types.map(pokemonType => `${ pokemonTypeApiUrl }/${ pokemonType.type.name }`)
+            : null,
+        multiFetcher
+    )
+}
