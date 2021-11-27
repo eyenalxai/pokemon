c5ba96d2384ef5d42230d6c97ba215a4e2a8eb26
Yest Izit <hi@dmitry.email>
2021-11-27 16:02:22 +0300

    I don't know why. Just move on.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default class MyDocument extends Document {
     override render() {
         return (
             <Html lang="en">
-                <body>
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2"/>
@@ -18,6 +17,7 @@ export default class MyDocument extends Document {
                     <meta name="theme-color" content="#1B202B" media="(prefers-color-scheme: dark)"/>
                     <meta name="description" content="Pokemon types and weaknesses checker."/>
                 </Head>
+                <body>
                 <Main/>
                 <NextScript/>
                 </body>
