39dcf6e35a1ce84b4eb707a602ba40dc360ae848
Yest Izit <hi@dmitry.email>
2021-12-02 21:38:18 +0300

    Make it work

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest): Promise<void | NextResponse> {
-    if (!["pokemon", "types", "attack"].includes(req.nextUrl.pathname.substring(1))) {
+    if (!["pokemon", "type", "attack"].includes(req.nextUrl.pathname.substring(1))) {
         return NextResponse.redirect("/pokemon")
     }
 }
